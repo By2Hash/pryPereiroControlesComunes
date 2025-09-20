7e12d9edb04eab83f640ba6213aa7d0ccbd7af14
By2Hash <by2git2@gmail.com>
2025-09-20 16:18:08 -0300

    Terminado el ejercicio

--- pryPereiroControlesComunes/Form1.Designer.cs
+++ pryPereiroControlesComunes/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pryPereiroControlesComunes
 {
-    partial class Form1
+    partial class frmEjemplo
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            chkRojo = new CheckBox();
+            chkAzul = new CheckBox();
+            chkAmarillo = new CheckBox();
+            lblEjemplo = new Label();
+            SuspendLayout();
+            // 
+            // chkRojo
+            // 
+            chkRojo.AutoSize = true;
+            chkRojo.Location = new Point(12, 12);
+            chkRojo.Name = "chkRojo";
+            chkRojo.Size = new Size(74, 29);
+            chkRojo.TabIndex = 0;
+            chkRojo.Text = "Rojo";
+            chkRojo.UseVisualStyleBackColor = true;
+            chkRojo.CheckedChanged += chkRojo_CheckedChanged;
+            // 
+            // chkAzul
+            // 
+            chkAzul.AutoSize = true;
+            chkAzul.Location = new Point(12, 66);
+            chkAzul.Name = "chkAzul";
+            chkAzul.Size = new Size(72, 29);
+            chkAzul.TabIndex = 1;
+            chkAzul.Text = "Azul";
+            chkAzul.UseVisualStyleBackColor = true;
+            chkAzul.CheckedChanged += chkAzul_CheckedChanged;
+            // 
+            // chkAmarillo
+            // 
+            chkAmarillo.AutoSize = true;
+            chkAmarillo.Location = new Point(12, 117);
+            chkAmarillo.Name = "chkAmarillo";
+            chkAmarillo.Size = new Size(104, 29);
+            chkAmarillo.TabIndex = 2;
+            chkAmarillo.Text = "Amarillo";
+            chkAmarillo.UseVisualStyleBackColor = true;
+            chkAmarillo.CheckedChanged += chkAmarillo_CheckedChanged;
+            // 
+            // lblEjemplo
+            // 
+            lblEjemplo.AutoSize = true;
+            lblEjemplo.BorderStyle = BorderStyle.Fixed3D;
+            lblEjemplo.Font = new Font("Segoe UI", 16F);
+            lblEjemplo.Location = new Point(207, 66);
+            lblEjemplo.Name = "lblEjemplo";
+            lblEjemplo.Size = new Size(267, 47);
+            lblEjemplo.TabIndex = 3;
+            lblEjemplo.Text = "Texto de Ejemplo";
+            // 
+            // frmEjemplo
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(512, 208);
+            Controls.Add(lblEjemplo);
+            Controls.Add(chkAmarillo);
+            Controls.Add(chkAzul);
+            Controls.Add(chkRojo);
+            Name = "frmEjemplo";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Prueba de Controles Comunes";
+            Load += frmEjemplo_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private CheckBox chkRojo;
+        private CheckBox chkAzul;
+        private CheckBox chkAmarillo;
+        private Label lblEjemplo;
     }
 }
--- pryPereiroControlesComunes/Form1.cs
+++ pryPereiroControlesComunes/Form1.cs
@@ -1,10 +1,80 @@
 namespace pryPereiroControlesComunes
 {
-    public partial class Form1 : Form
+    public partial class frmEjemplo : Form
     {
-        public Form1()
+        public frmEjemplo()
         {
             InitializeComponent();
         }
+
+        private void frmEjemplo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkRojo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkRojo.Checked == true)
+            {
+                lblEjemplo.ForeColor = Color.Red;
+                if (chkAzul.Checked == true)
+                {
+                    lblEjemplo.ForeColor = Color.Violet;
+                }
+                if (chkAmarillo.Checked == true)
+                {
+                    lblEjemplo.ForeColor = Color.Orange;
+                }
+            }
+            else
+            {
+                lblEjemplo.ForeColor = Color.Black;
+            }
+        }
+
+        private void chkAzul_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAzul.Checked == true)
+            {
+                lblEjemplo.ForeColor = Color.Blue;
+                if (chkRojo.Checked == true)
+                {
+                    lblEjemplo.ForeColor = Color.Violet;
+                }
+                if (chkAmarillo.Checked == true)
+                {
+                    lblEjemplo.ForeColor = Color.Green;
+                }
+            }
+            else
+            {
+                lblEjemplo.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void chkAmarillo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAmarillo.Checked == true)
+            {
+                lblEjemplo.ForeColor = Color.Yellow;
+                if(chkAzul.Checked == true)
+                {
+                    lblEjemplo.ForeColor = Color.Green;
+                }
+                if(chkRojo.Checked == true)
+                {
+                    lblEjemplo.ForeColor = Color.Orange;
+                }
+            }
+            else
+            {
+                lblEjemplo.ForeColor = Color.Black;
+            }
+
+            }
+        }
     }
-}
+
+    
+
